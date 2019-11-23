3804e57b29e2c94f731cb49479395d2f26d9f2c9
vvbudh <vvbudh@gmail.com>
2019-11-23 10:51:47 -0800

    Got the data to cross over and understand it, just need to make the main menu for it. But I'm going to play a game and take a break. It's been two hours and that includes cooking my breakfast.

--- Fun/Scraps/EchoClient.py
+++ Fun/Scraps/EchoClient.py
@@ -1,7 +1,40 @@
 import socket
-message = "This is a message."
 
-HOST = 127.0.0.1
-PORT = 55555
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientToServer
+def communicateToServer():
+    """This function communicates to the server sending 10
+    pieces of data. That data is "The local variable below. It will also wait
+    for recieving data"""
+    message = "This is a message."
+    i = 0
+    HOST = '127.0.0.1'
+    PORT = 55555
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST,PORT))
+        print("Line is open. s.connect!")
+        while i <= 10:
+            s.send(message.encode(encoding='utf-8', errors='strict'))
+            i = i + 1
+        print("Encoded and sent ", i)
+        incomingMessage = s.recv(1024)
+
+
+    print(incomingMessage)
+
+
+def mainMenu():
+    stayon = True
+    while stayon == True:
+        for i in range(0,100): print("\n")
+        print("Yo, here's the menu options for tonight.")
+        print("1 send 1 data packet. \n 0 exit the program.")
+        menuSelection = input("Pick your poison.")
+        if menuSelection == 1:
+            communicateToServer()
+        if menuSelection == 0:
+            stayon = False
+            break
+
+
+################################################################################
+mainMenu()
--- Fun/Scraps/EchoServer.py
+++ Fun/Scraps/EchoServer.py
@@ -3,18 +3,16 @@ import socket
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 55555        # Port to listen on (non-privileged ports are > 1023)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as listenSocket:
-    listenSocket.bind((HOST, PORT))
-    listenSocket.listen()
-    outsideConnection, addr = listenSocket.accept()
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    #conn, addr = s.accept()  # Establish connection with client.
+    with conn:
+            print('Connected by', addr)
+            while conn:
+                incomingMessage = conn.recv(1024).decode(encoding='utf-8',
+                                                          errors='strict')
+                print(incomingMessage)
+            conn.send("Receipt of message, Closing connection.")
 
-    with outsideConnection:
-        print('Connected by', addr)
-        while True:
-            data = int(outsideConnection.recv(1024))
-            if not data:
-                break
-            data = data + 5
-            outsideConnection.send(bytes(data))
-            #outsideConnection.sendall(data)#send the data back to the client
-            print(data)
