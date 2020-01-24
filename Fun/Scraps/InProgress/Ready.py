0e828cf8cc78f6233d3d21100ec954e32ad3d745
vvbudh <vvbudh@gmail.com>
2020-01-24 08:12:50 -0800

    Started a new program that will talk to me.

---
+++ Fun/Scraps/InProgress/Ready.py
@@ -0,0 +1,11 @@
+'''The talk to me program. I have few people to talk to, so....I'll make the
+computer talk to me!'''
+
+print("PC:Hello!")
+userInput = input("Would you like to say 'Hi' back?\n"
+                  "1) Hello.\n"
+                  "2) Goodbye.\n"
+                  "3) Hello, How are you?\n")
+
+if userInput == 1:
+    print("Hello")
