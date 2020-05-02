c03279441d59d49348d5814f88c8178f2e86c961
tylort69 <porsht@hotmail.com>
2020-05-02 14:45:40 -0400

    Update it's_Like_Art.py

--- Fun/Scraps/InProgress/it's_Like_Art.py
+++ Fun/Scraps/InProgress/it's_Like_Art.py
@@ -1,16 +1,32 @@
-import sys
-import os
-import socket
-import random as r
 
+'''
+n! means n × (n − 1) × ... × 3 × 2 × 1
 
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
 
-inverse = 100
-multiplied = 001
+Find the sum of the digits in the number 100!
+'''
+import math
 
-
-if (inverse) == multiplied:
-    print("We found one. ", multiplied)
-    incriment = -1
-
-
+flag = False
+son = 0
+n = 0
+p = 0
+progress = list()
+type(progress)
+goal = 100
+#while will run until goal is satisfied
+while flag == False:
+    progress.append(n)
+    numberarray = [int(x) for x in str(p)]
+    #add numbers from factorial product
+    nalength = (len(numberarray-1)
+    for i in range (0,nalength):
+        son+=numberarray[i]
+        i+=1
+    #if sum == goal yay!
+    if son == goal:
+        print("the factorial thats sum eqauls 100 is", n)
+        flag = True
+    n+=1
