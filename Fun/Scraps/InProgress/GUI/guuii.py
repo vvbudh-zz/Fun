20b1043129fa275f1c7de65d6f035650d4b0b97f
tylort69 <porsht@hotmail.com>
2020-05-02 18:37:01 -0400

    Update guuii.py

---
+++ Fun/Scraps/InProgress/GUI/guuii.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+window = tk.Tk()
+greeting = tk.Label(text="Hello, Tkinter")
+greeting.pack()
+window.mainloop()
+label = tk.Label(text="Hello, Tkinter")
+label = tk.Label(
+    text="Hello, Tkinter",
+    foreground="white",  # Set the text color to white
+    background="black"  # Set the background color to black
+)
